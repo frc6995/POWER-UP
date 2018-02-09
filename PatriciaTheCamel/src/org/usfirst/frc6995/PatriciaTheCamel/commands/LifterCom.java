1ecc6e0b6fd00c2955566f28be8bcaadb6b5e64c
Greg Shue <greg.shue@outlook.com>
2018-02-09 13:47:57 -0800

    LifterCom: Add incremental movement logic (no real constraints yet)

--- PatriciaTheCamel/src/org/usfirst/frc6995/PatriciaTheCamel/commands/LifterCom.java
+++ PatriciaTheCamel/src/org/usfirst/frc6995/PatriciaTheCamel/commands/LifterCom.java
@@ -11,7 +11,22 @@ import org.usfirst.frc6995.PatriciaTheCamel.Robot;
  */
 public class LifterCom extends Command {
 
+	static final int ENCODER_COUNTS_PER_INCH = 100; //REVISIT - What is the real encoding resolution?
+	static final int RISER_STOP_DIST_ENC = 25;  // TODO: Need real numbers
+	static final int ENCODER_COUNTS_PER_ROTATION = 1000; //REVISIT - What is the real encoding resolution?
+	static final int ROTATOR_STOP_DIST_ENC = 2;  // TODO: Need real numbers
+
+	// Though we don't know the exact interference heights until the mechanics are built,
+	// we do know what the causes of interference are and can predict many of the breakpoints
+	// in our mech interference logic.  Mostly we want to know when we can start
+	//
+	// - We know that at the very bottom position the ground and the conveyor will interfere with the rotation.
+	// - We know that the ground will be the first interference to go away.
+	// - We know that we want to be able to grab a box on the conveyor at 45 deg up.
+	static final int RISER_GROUND_CLEARANCE_pos135_ENC = (int) (5.0 * ENCODER_COUNTS_PER_INCH);
+	
 	static final int OVERRIDE_ANGLE_INVALID = -1000;
+	
 	static int overrideAngleEnc = OVERRIDE_ANGLE_INVALID;
 	
 	private int destHeightEnc;
@@ -31,6 +46,7 @@ public class LifterCom extends Command {
 		}
 		
 	}
+	
 	
 	public LifterCom(double destHeightInch, double destAngleDeg) {
 		
@@ -79,13 +95,78 @@ public class LifterCom extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+
+    	final int currHeightEnc = 0; // TODO: Need to read current height
+    	final int currAngleEnc = 0; // TODO: Need to read current angle
     	
-    	// Support for override angle control
+    	int nextHeightEnc = this.destHeightEnc;  // Manual control directly updates this.destHeightEnc
     	int nextAngleEnc = this.destAngleEnc;
+
+    	// Support for override angle control
     	if (this.enableAngleOverride && OVERRIDE_ANGLE_INVALID != overrideAngleEnc) {
     		// Head to this angle instead of the standard one
     		nextAngleEnc = overrideAngleEnc;
     	}
+    	
+    	// Now that we know where we want to get to
+    	// we need to figure out how to get there (and if we can)
+    	
+    	if (nextHeightEnc != currHeightEnc) {
+    	   	if (nextHeightEnc > currHeightEnc) {
+        		// Then we need to go higher - unless we need to get out of a keep-out zone
+        		nextHeightEnc += Math.min(nextHeightEnc - currHeightEnc, RISER_STOP_DIST_ENC);
+        	}
+        	else {
+        		// Then we need to go lower - always safe
+        		nextHeightEnc -= Math.min(currHeightEnc - nextHeightEnc, RISER_STOP_DIST_ENC);
+        	}
+    	}
+     	else {
+        	// We have arrived at the right height. No adjustment needed.
+    	}
+    	
+    	// Now we have to check constraints.
+    	//  a) We can only adjust our height iff no mech interference
+    	//  b) We must adjust our angle to a position safe to enter the new height
+    	//  NOTE: In our mech the only place we know of that we may have to adjust
+    	//        our height to get to a safe angle is up at the very top.
+    	//        We can use a sequence move to navigate in to pre-climb position
+    	//        and avoid the complexity of a more general solution.
+    	
+    	// TODO Call a function to determine constraints at the requested height.
+    	
+    	int minAngleEnc = rotatorMinAngleEnc(nextHeightEnc);
+    	int maxAngleEnc = rotatorMaxAngleEnc(nextHeightEnc);
+
+    	// TODO Figure out how to apply the keep out region
+    	//int minAngleKeepOutEnc = rotatorMinAngleKeepOutEnc(nextHeightEnc);
+    	//int maxAngleKeepOutEnc = rotatorMaxAngleKeepOutEnc(nextHeightEnc):
+
+    	// Limit the next angle based on constraints
+    	nextAngleEnc = Math.max(minAngleEnc, Math.min(nextAngleEnc, maxAngleEnc));	
+    	
+    	// Update next angle request
+    	if (nextAngleEnc != currAngleEnc) {
+        	if (nextAngleEnc > currAngleEnc) {
+        		// Then we need to go out - unless we can't due to a constraint
+        		nextAngleEnc += Math.min(nextAngleEnc - currAngleEnc, ROTATOR_STOP_DIST_ENC);
+        	}
+        	else if (nextAngleEnc < currAngleEnc) {
+        		// Then we need to go in
+        		nextAngleEnc -= Math.min(currAngleEnc - nextAngleEnc, ROTATOR_STOP_DIST_ENC);
+        	}
+     	}
+    	else {
+    		// We have arrived at the right angle. No adjustment needed.
+    	}
+    	// TODO Go to new angle
+
+    	// If it is safe to change height, then do so.
+    	if (currAngleEnc <= maxAngleEnc && currAngleEnc >= minAngleEnc &&
+    			nextAngleEnc <= maxAngleEnc && nextAngleEnc >= minAngleEnc) {
+    		// TODO Go to new height
+    	}
+     	
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -108,18 +189,34 @@ public class LifterCom extends Command {
     
     protected static int riserInchToEnc(double heightInch) {
     	double limitedHeightInch = Math.max(0.5, Math.min(heightInch, 81.0));
-    	final int ENCODER_COUNTS_PER_INCH = 100; //REVISIT - What is the real encoding resolution?
     	return (int) (limitedHeightInch * ENCODER_COUNTS_PER_INCH);
     }
     
     protected static int rotatorDegToEnc(double angleDeg) {
     	double limitedAngleDeg = Math.max(-150.0, Math.min(angleDeg, 150.0));
-    	final int ENCODER_COUNTS_PER_ROTATION = 1000; //REVISIT - What is the real encoding resolution?
     	return (int) (limitedAngleDeg * ENCODER_COUNTS_PER_ROTATION / 360);
     }
     
     // Called to request a destination angle override
     public static void overrideDestAngleDeg(double destAngleDeg) {
     	overrideAngleEnc = rotatorDegToEnc(destAngleDeg);
+    }
+    
+    // Called to get the constraint angles for a specific height.
+    // Though it would be more Java-like to return these in a class
+    // we ought to do our best to avoid dynamic allocation to avoid 
+    // uncontrolled garbage collection.  This means we might as well
+    // provide separate functions for each constraint.  (They will each
+    // return different values anyway.
+    public static int rotatorMinAngleEnc(int heightEnc) {
+    	return 0;  // TODO Need real values
+    }
+    
+    public static int rotatorMaxAngleEnc(int heightEnc) {
+    	
+    	if (RISER_GROUND_CLEARANCE_pos135_ENC >= heightEnc) {
+    		return 90;  // TODO Need real values
+    	}
+		return 150;  // TODO Need real values
     }
 }
