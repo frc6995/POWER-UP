2743580d52d4b3ef1091e9b6d1ae613b7bb849b3
J.A. Shue <j.a.shue@outlook.com>
2018-02-11 13:22:08 -0800

    fixed limit switch logic and changed speed of conveyor

--- PatriciaTheCamel/src/org/usfirst/frc6995/PatriciaTheCamel/commands/ConveyorOut.java
+++ PatriciaTheCamel/src/org/usfirst/frc6995/PatriciaTheCamel/commands/ConveyorOut.java
@@ -45,7 +45,7 @@ public class ConveyorOut extends Command {
     protected void initialize() {
     	cycles_left = cycles_out_max;
     	if (0 < cycles_left) {
-    		Robot.conveyor.conveyorMotor.set(-0.0625);
+    		Robot.conveyor.conveyorMotor.set(-0.1875);
     	}
     }
 
