defc6a8fab7b13429cadb2a3d7530a2811b7641a
J.A. Shue <j.a.shue@outlook.com>
2018-02-14 20:09:17 -0800

    Lifter: Enable Hat Dn + manual riser buttons increase/decrease rotator zero location.

--- PatriciaTheCamel/src/org/usfirst/frc6995/PatriciaTheCamel/commands/RiserReqMonitor.java
+++ PatriciaTheCamel/src/org/usfirst/frc6995/PatriciaTheCamel/commands/RiserReqMonitor.java
@@ -7,4 +7,12 @@ public interface RiserReqMonitor {
 	// A zero return value causes the riser to stop.
 	// A positive return value causes the riser to raise.
 	int riserRequest();
+	
+	// Function called in execute cycle to determine the
+	// direction to adjust the rotator zero position.
+	// A negative return value causes the fixed positions
+	// to move farther out from the robot. A positive return value
+	// causes the fixed positions to move farther in to the robot.
+	// A zero return causes no change to the fixed positions.
+	int rotatorZeroAdjustRequest();
 }
